1b97b1f47fadf7ed912d2545afff392d36087022
papanpapan <samim.skmolla@gmail.com>
2017-03-15 18:17:57 +0530

    Result
    Result

---
+++ src/result.java
@@ -0,0 +1,21 @@
+
+public class result {
+
+	
+	public static void main(String[] args) {
+		int a=20;
+		int b=10;
+		
+		
+		b= a-- - --a;
+		System.out.println("the result B is:" +b+"\n");
+		int c=a--;
+		System.out.println("the result C is:" +c+"\n");
+		int d=a>>2;
+		System.out.println("the result D is:" +d+"\n");
+		int e=a&b;
+		System.out.println("the result e is:" +e);
+
+	}
+
+}
